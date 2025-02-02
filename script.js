8f18ef8fab94e045e99a683e47bd9501640e4ff8
Zico-X <monumidu115@gmail.com>
2025-02-02 16:56:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function goToShop() {
     window.location.href = 'flip.html';  // Make sure 'flip.html' is in the same directory as your index.html file.
 }
+ const menuIcon = document.querySelector('.menu-icon');
+        const menu = document.querySelector('nav ul');
+
+        menuIcon.addEventListener('click', () => {
+            menu.classList.toggle('show');
+        });
